104d9b0924d680fbda820f9334bb353c06c2364f
ddekanosidze <44446303+ddekanosidze@users.noreply.github.com>
2020-03-30 15:42:43 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -36,13 +36,27 @@
 //         console.log(data[i].name)
 // }
 //     })
-    let container = document.querySelectorAll('.container');
-    for(let i = 0 ;i < container.length;i++ ){
-        container[i].addEventListener("click",function(e){
-            if(e.target.innerText == ''){
-                return
-            }
-        })
-       }
+let submitSendButton = document.querySelector(".submit-button");
+let submitInformationPopup = document.querySelector(".submit-information-popup");
 
+submitSendButton.addEventListener("click",showHidePopup)
 
+let container = document.querySelectorAll('.container');
+for(let i = 0 ;i < container.length;i++ ){
+    container[i].addEventListener("click",function(e){
+        if(e.target.innerText == ''){
+            return
+        }
+    })
+}
+
+function showHidePopup(){
+    submitInformationPopup.style.display = "block"
+    submitInformationPopup.style.opacity = "1"
+    submitInformationPopup.style.transition ="0s ease-in-out";
+
+    setTimeout(() => {
+        submitInformationPopup.style.opacity = "0"
+        submitInformationPopup.style.transition ="2s ease-in-out";
+    }, 2000);
+}
